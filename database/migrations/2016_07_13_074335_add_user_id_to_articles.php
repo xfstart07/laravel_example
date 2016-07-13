25036aa0bddb011fe91b4999a2c7e4ae23f5f64f
Leon Xu <xfstart07@gmail.com>
2016-07-13 15:56:49 +0800

    Add articles

---
+++ database/migrations/2016_07_13_074335_add_user_id_to_articles.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUserIdToArticles extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            $table->integer('user_id')->index()->after('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            //
+        });
+    }
+}
--- readme.md
+++ readme.md
@@ -1,6 +1,20 @@
 # Laravel for blog
 
+## 目录结构
+
+app  放 Model 的地方
+bootstrap 放 引导程序
+config 放配置文件
+database 放 数据迁移 和 填充数据 的程序
+resources 放 资源文件
+
 ##  数据迁移
+
+### 创建数据迁移
+
+    php artisan make:migration add_user_id_to_articles --table=articles
+
+`--table` 和 `--create` 选项可用来指定数据表的名称
 
 ### 运行数据迁移
 
@@ -10,6 +24,11 @@
 
     php artisan migrate:rollback
 
+## Model
+
+### 创建 model 同时生成 数据迁移
+
+    php artisan make:model User -m
 
 ## License
 
